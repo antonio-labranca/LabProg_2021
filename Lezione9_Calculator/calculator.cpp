8785240978c078646e035d9e7b8e404de09ccd11
Pietro Ferrara <pietro.ferrara@unive.it>
2021-03-12 10:18:41 +0100

    Lecture 10, evolving the calculator

--- Lezione9_Calculator/calculator.cpp
+++ Lezione9_Calculator/calculator.cpp
@@ -2,56 +2,54 @@
 
 using namespace std;
 
+struct unknown_operation_exception
+{
+   char op;
+};
 
-
-int stringToIntFromLeft(string s) {
-    int result = 0;
-   for(int i = 0; i < s.length(); i++) {
-      int digit = s.at(i)-'0';
-      result = result*10 + digit;  
+double performOperation(double left, double right, char op)
+{
+   double result;
+   switch (op)
+   {
+   case '+':
+      result = left + right;
+      break;
+   case '-':
+      result = left - right;
+      break;
+   case '*':
+      result = left * right;
+      break;
+   case '/':
+      result = left / right;
+      break;
+   default:
+      throw unknown_operation_exception{op};
    }
    return result;
-}
-
-int stringToIntFromRight(string s) {
-   int j = 1;
-   int result = 0;
-   for(int i = s.length()-1; i >= 0 ; i--) {
-      int digit = s.at(i)-'0';
-      result += digit*j;
-      j = j * 10;
-   }
-   return result;
-}
-
-int perforOperation(int left, int right, char op) {
-    if(op=='+')
-      return left+right;
-
 }
 
 int main()
 {
    //input: 3+5 //no white space
    //output: 8
-   string input;
-   cin >> input;
-
-   int i = 0;
-   while(input.at(i) >= '0' && input.at(i) <= '9')
-      i++;
-   string leftNumber = input.substr(0, i);
-
-   char op = input.at(i);
-
-   string rightNumber = input.substr(i+1, input.length()-i-1);
-
-   int leftNumberInt = stringToIntFromLeft(leftNumber);
-   int rightNumberInt = stringToIntFromRight(rightNumber);
-
-   int result = perforOperation(leftNumberInt, rightNumberInt, op);
-
-   cout << "L'input e' " << input << endl;
-   cout << "Il risultato e' " << result << endl;
+   double left, right;
+   char op;
+   cin >> left >> op;
+   double result = left;
+   try
+   {
+      while(op!='=') {
+         cin >> right;
+       result = performOperation(result, right, op);
+       cin >> op;
+      }
+      cout << "Il risultato e' " << result << endl;
+   }
+   catch (unknown_operation_exception e)
+   {
+      cout << "Operatore " << op << " sconosciuto nell'espressione " << endl;
+   }
    return 0;
 }
