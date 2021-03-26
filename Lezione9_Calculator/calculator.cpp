3b4e3c390a30ddb95ecd02c0ffd67226277fe6b0
Pietro Ferrara <pietro.ferrara@unive.it>
2021-03-26 10:21:02 +0100

    removing old code

--- Lezione9_Calculator/calculator.cpp
+++ Lezione9_Calculator/calculator.cpp
@@ -3,58 +3,6 @@
 
 using namespace std;
 
-struct unknown_operation_exception
-{
-   char op;
-};
-
-double performOperation(double left, double right, char op)
-{
-   double result;
-   switch (op)
-   {
-   case '+':
-      result = left + right;
-      break;
-   case '-':
-      result = left - right;
-      break;
-   case '*':
-      result = left * right;
-      break;
-   case '/':
-      result = left / right;
-      break;
-   default:
-      throw unknown_operation_exception{op};
-   }
-   return result;
-}
-/*
-int main()
-{
-   //input: 3+5 //no white space
-   //output: 8
-   double left, right;
-   char op;
-   cin >> left >> op;
-   double result = left;
-   try
-   {
-      while(op!='=') {
-         cin >> right;
-         result = performOperation(result, right, op);
-         cin >> op;
-      }
-      cout << "Il risultato e' " << result << endl;
-   }
-   catch (unknown_operation_exception e)
-   {
-      cout << "Operatore " << op << " sconosciuto nell'espressione " << endl;
-   }
-   return 0;
-}*/
-
 struct Token {
   bool isNumber;
   double value;
@@ -167,7 +115,7 @@ double number() {
 int main() {
    read_input();
    double result = expression();
-   cout << result;
+   cout << result << endl;
    return 0;
 
 }
